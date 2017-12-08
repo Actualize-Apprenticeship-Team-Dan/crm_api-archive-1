e0eee2ecb4868d1f46dd106c735e7b45e92d0d00
xthegoodguy <xashinx@gmail.com>
2017-12-07 20:01:35 -0600

    201 - Worked on history of leads

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -16,6 +16,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
     methods: {
       moment: function(date) {
         return moment(date);
+      },
+      showEvents: function(lead) {
+        if ($("#" + lead.id + "-events").length === 0) { 
+          var eventsString = "";
+          for (event of lead.events) {
+            eventsString += "<p>" + event.name + "</p>";
+          };
+          var $row = $('#lead-' + lead.id);
+          if (lead.events.length === 0) {
+            var idString = '' + lead.id + '-events';
+            var $newRow = $('<tr id=' + idString + '><td colspan="7">EVENTS: No Events</td></tr>');
+          } else {
+            var idString = '' + lead.id + '-events';
+            var $newRow = $('<tr id=' + idString + '><td colspan="7">EVENTS: ' + eventsString + "</td></tr>");
+          };
+          $row.after($newRow);
+        }
       }
     },
     computed: {
