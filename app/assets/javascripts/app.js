cae2007577d30f667db326973e42fdccdaadd2f3
xthegoodguy <xashinx@gmail.com>
2017-12-11 18:53:11 -0600

    201 - Created toggle function for events and dates for the event for each lead

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -18,20 +18,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
         return moment(date);
       },
       showEvents: function(lead) {
-        if ($("#" + lead.id + "-events").length === 0) { 
+        var $eventRow = $("#" + lead.id + "-events");  //creates a string with a lead id.
+        if ($eventRow.length === 0) { 
           var eventsString = "";
           for (event of lead.events) {
-            eventsString += "<p>" + event.name + "</p>";
-          };
+            eventsString += '<div class="row">';
+            eventsString += '<div class="col-md-2">' + event.name + '</div>';
+            eventsString += '<div class="col-md-4">' + this.moment(event.created_at).format('dddd MMM Do YYYY, h:mm a') + '</div>';
+            eventsString += '</div>';
+          };                                  // this loop double checks whether or not the event exists already, if it doesn't it will create a string into the html.
           var $row = $('#lead-' + lead.id);
           if (lead.events.length === 0) {
             var idString = '' + lead.id + '-events';
             var $newRow = $('<tr id=' + idString + '><td colspan="7">EVENTS: No Events</td></tr>');
           } else {
             var idString = '' + lead.id + '-events';
-            var $newRow = $('<tr id=' + idString + '><td colspan="7">EVENTS: ' + eventsString + "</td></tr>");
-          };
+            var $newRow = $('<tr id=' + idString + '><td colspan="7">' + eventsString + "</td></tr>");
+          };                    // no events == no events
           $row.after($newRow);
+        } else {
+          $eventRow.remove(); // if events row exists then remove tho row(toggle). 
         }
       }
     },
