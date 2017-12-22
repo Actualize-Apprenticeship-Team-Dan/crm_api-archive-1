ace77ee4ea7e5fcee159e3ccbc72ff0172b61594
FirstofHerName <mbe773@gmail.com>
2017-12-21 20:01:08 -0600

    272-adjusting forms to be more dynamic need to set variable to 'event boolean' to TRUE still need to add auto-text button

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         time_format: "12/25/17",
         url: "https://www.google.com/",
         search: '',
-        reverse: 1
+        reverse: 1,
       };
     },
     mounted: function() {
@@ -31,34 +31,48 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
       },
       showEvents: function(lead) {
-        //creates a string with a lead id.
-        if (!lead) { return; }
-        var $eventRow = $("#" + lead.id + "-events");  
-        if ($eventRow.length === 0) { 
-          var eventsString = "";
-          for (event of lead.events) {
-            eventsString += '<div class="row">';
-            eventsString += '<div class="col-md-2">' + event.name + '</div>';
-            eventsString += '<div class="col-md-4">' + this.moment(event.created_at).format('dddd MMM Do YYYY, h:mm a') + '</div>';
-            // this loop double checks whether or not the event exists already, if it doesn't it will create a string into the html.
-            eventsString += '</div>';
-          };                                  // this loop double checks whether or not the event exists already, if it doesn't it will create a string into the html.
-          var $row = $('#lead-' + lead.id);
-          if (lead.events.length === 0) {
-            var idString = '' + lead.id + '-events';
-            var $newRow = $('<tr id=' + idString + '><td colspan="7">EVENTS: No Events</td></tr>');
-          } else {
-            var idString = '' + lead.id + '-events';
-            var $newRow = $('<tr id=' + idString + ' class="event-row"><td colspan="7">' +
-              // no events == no events 
-              eventsString + "</td></tr>");
-          };                
-          $row.after($newRow);
+        var r = document.getElementById("event-${lead.id}");
+        eventBoolean = !eventBoolean;
+        if (eventBoolean === true) {
+          // r.className = "event-display";
+          console.log("TRUE");
+          console.log(eventBoolean);
         } else {
-          // if events row exists then remove tho row(toggle).
-          $eventRow.remove();  
+          console.log("false");
         }
-      }, 
+      },
+      // autoText: function(lead){
+      //   console.log("Hello")
+      // },
+      // showEvents: function(lead) {
+      //   //creates a string with a lead id.
+      //   if (!lead) { return; }
+      //   var $eventRow = $("#" + lead.id + "-events");  
+      //   if ($eventRow.length === 0) { 
+      //     var eventsString = "";
+      //     for (event of lead.events) {
+      //       eventsString += '<div class="row">';
+      //       eventsString += '<div class="col-md-2">' + event.name + '</div>';
+      //       eventsString += '<div class="col-md-4">' + this.moment(event.created_at).format('dddd MMM Do YYYY, h:mm a') + '</div>';
+      //       // this loop double checks whether or not the event exists already, if it doesn't it will create a string into the html.
+      //       eventsString += '</div>';
+      //     };                                  // this loop double checks whether or not the event exists already, if it doesn't it will create a string into the html.
+      //     var $row = $('#lead-' + lead.id);
+      //     if (lead.events.length === 0) {
+      //       var idString = '' + lead.id + '-events';
+      //       var $newRow = $('<tr id=' + idString + '><td colspan="6">EVENTS: No Events</td><td><button class="btn btn-primary">Auto-text</button></td></tr>');
+      //     } else {
+      //       var idString = '' + lead.id + '-events';
+      //       var $newRow = $('<tr id=' + idString + ' class="event-row"><td colspan="6">' +
+      //         // no events == no events 
+      //         eventsString + "</td><td><button id='auto-text' onClick='app.autoText(lead)' class='btn btn-primary'>Auto-text</button></td></tr>");
+      //     };                
+      //     $row.after($newRow);
+      //   } else {
+      //     // if events row exists then remove tho row(toggle).
+      //     $eventRow.remove();  
+      //   }
+      // }, 
       rowColor: function(lead){
         
         if (!lead.outreaches.length){
